e0bf9ba23cff4d3fe90b4ec5734e826b6cf4c551
nimeshdutt <nimeshdutt@nimeshdutts-MacBook-Air.local>
2024-09-28 11:06:31 +0530

    initial commit

---
+++ frontend/src/pages/customer/pages/CheckoutAftermath.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { Container, Paper, Typography } from "@mui/material";
+import { LightPurpleButton } from "../../../utils/buttonStyles";
+import { KeyboardDoubleArrowLeft } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
+const CheckoutAftermath = () => {
+  const navigate = useNavigate();
+
+  return (
+    <React.Fragment>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <React.Fragment>
+            <Typography variant="h5" gutterBottom>
+              Thank you for your order.
+            </Typography>
+            <Typography variant="subtitle1">
+              Your order has been successful. We have emailed your order
+              confirmation, and will send you an update when your order has
+              shipped.
+            </Typography>
+            <LightPurpleButton
+              sx={{ mt: 10 }}
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              <KeyboardDoubleArrowLeft /> Back to Home
+            </LightPurpleButton>
+          </React.Fragment>
+        </Paper>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default CheckoutAftermath;
---
+++ frontend/src/pages/seller/pages/ShopcartSpecial.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShopcartSpecial = () => {
+  return <div>ShopcartSpecial</div>;
+};
+
+export default ShopcartSpecial;
